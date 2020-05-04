df253613ab6905d63ade6505d69ecadb27bcfa86
Fung Yung <fungyung1225@gmamil.com>
2020-05-04 12:03:14 -0400

    added IBOutlet and IBAction for the bins and lab

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var recordBtn: UIButton!
+    @IBOutlet var msgLab: UILabel!
+    @IBOutlet var stopBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("view will appear")
+    }
+    
+    @IBAction func recordAction(_ sender: UIButton) {
+        msgLab.text = "recording, press stop btn to stop record"
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+    }
 }
 
