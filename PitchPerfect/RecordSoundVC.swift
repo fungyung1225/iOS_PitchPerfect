b95eda347961035ca1ce3e0446303bad27fb3c82
Fung Yung <fungyung1225@gmamil.com>
2020-05-04 19:09:51 -0400

    implemented start and stop record functions

--- PitchPerfect/RecordSoundVC.swift
+++ PitchPerfect/RecordSoundVC.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundVC: UIViewController {
+    var audioRecorder: AVAudioRecorder!    //declare av recorder variable
+    
     @IBOutlet var recordBtn: UIButton!
     @IBOutlet var msgLab: UILabel!
     @IBOutlet var stopBtn: UIButton!
@@ -41,15 +44,31 @@ class RecordSoundVC: UIViewController {
         msgLab.text = "tap to record"
         recordBtn.isEnabled = true
         stopBtn.isEnabled = false
+        
+        stopRecord()
     }
     
     func startRecord(){
         print("start record func")
-        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath as Any)
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     func stopRecord(){
         print("stop record func")
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
 }//end of class
 
